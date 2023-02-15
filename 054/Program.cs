32eabcc003e67423089c733af596dbcb78e1f8d2
«Viktoria» <vika.mel.2015@mail.ru>
2023-02-15 23:16:12 +0300

    ДЗ №3

---
+++ 054/Program.cs
@@ -0,0 +1,10 @@
+﻿//54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+int a=0;
+int b=2;
+System.Console.Write("a=");
+a=Convert.ToInt32(Console.ReadLine());
+
+
+if (b>a*a && a<2)
+         System.Console.WriteLine("Являются равным   0 и 1"); 
+    else System.Console.WriteLine("Не являются равным  0 и 1");
---
+++ 056/Program.cs
@@ -0,0 +1,12 @@
+﻿// 56.Написать программу копирования массива
+
+int[] a= {3, 2, 7, 1, 9,3,8};
+
+PrintArray(a);
+
+void PrintArray(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i]} ");
+}
+
