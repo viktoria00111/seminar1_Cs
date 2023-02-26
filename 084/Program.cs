b6c7c1bba06d7a052bb5e1b735789a10faeccab0
«Viktoria» <vika.mel.2015@mail.ru>
2023-02-26 17:42:25 +0300

    ДЗ 8

---
+++ 084/Program.cs
@@ -0,0 +1,26 @@
+﻿//84.Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа
+using System;
+                    
+public class Program
+{
+    public static void Main() {
+        string number = Console.ReadLine();
+        int sum = 0;
+        for (int i = 0; i < number.Length; i++) {
+            if (char.IsDigit(number[i])) {
+                sum += number[i] - 48;
+            }
+            else {
+                // пропуск минуса в начале, если он есть
+                if (i == 0 && number[i] == '-') {
+                    continue;
+                }
+                else {
+                    Console.WriteLine("Это не является верной записью целого");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("Сумма цифр числа: {0}", sum);
+    }
+}
---
+++ 086/Program.cs
@@ -0,0 +1,32 @@
+﻿// 86.Подсчитать сколько раз определенный символ (например 'a') встречается в строке.
+
+using System.Text;
+string s = "";
+Console.Write("Вводим строку: ");
+s=Console.ReadLine();
+StringBuilder t = new StringBuilder(s);
+for (int i = 0; i < t.Length; i++) // По каждому символу
+{
+    char c = t[i];  // Записываем отдельно каждый символ для сравнение
+    int k = 1; // Счётчик
+    for (int j = i+1; j < t.Length; j++) // Ищем совпадения
+    {
+        if (c==t[j]) // Если символ повторяется
+        {
+            k++; // Увеличиваем счётчик
+            t.Remove(j, 1); // Удаляем символ
+            j--; // т.к. удалили символ
+        }
+    }
+    Console.WriteLine($" {c} - {k} раз");
+}
+t = new StringBuilder(s);
+if (t.Length >= 8)
+    for (int i = 0; i < t.Length - 7; i++) // Ищем слово "проблема"
+        if ((t[i] == 'п') & (t[i + 1] == 'р')& (t[i+2] == 'о') & (t[i+3] == 'б') & (t[i+4] == 'л') & (t[i+5] == 'е') & (t[i+6] == 'м') & (t[i+7] == 'а'))
+        {
+            t.Remove(i, 8); // Удаляем слово
+            i--;
+        }
+Console.WriteLine(t);
+
